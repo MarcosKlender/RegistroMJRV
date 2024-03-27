ac50ea6ca7845f4dc2188601caf078ce29f354d4
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2024-03-26 21:00:56 -0500

    Readme updated.
    Visual fixes on report view.

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # RegistroMJRV
  Web app for the tracking and registration of +8600 MJRV.
+
+## Data for Excel Imports
+Please make sure your Excel files have the following columns:
+
+#### MJRV
+
+- provincia
+- canton
+- parroquia
+- zona
+- recinto
+- institucion
+- junta
+- sexo
+- cedula
+- nombre
+- correo
+- correo2
+- celular
+
+#### Users
+
+- name
+- email
+- username
+- phone
+- location
--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -10,18 +10,24 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
-
-                    <div class="flex flex-col md:flex-row md:justify-between md:items-center mx-auto">
-                        <div class="basis-1/3 font-bold">
-                            {{ __('TOTAL DE MJRV REGISTRADOS:') }} {{ $membersCount }}
+                    @if ($membersCount == 0)
+                        <div class="text-center font-bold py-3">
+                            {{ __('Ninguna asistencia ha sido registrada todavía.') }}
                         </div>
+                    @else
+                        <div class="flex flex-col md:flex-row md:justify-between md:items-center mx-auto">
+                            <div class="basis-1/3 text-center md:text-left font-bold pb-5 md:pb-0">
+                                {{ __('TOTAL DE MJRV REGISTRADOS:') }} {{ $membersCount }}
+                            </div>
 
-                        <div class="flex flex-row justify-center">
-                            <a href="{{ route('mjrv.export') }}"
-                                class="block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                Generar Reporte</a>
+                            <div class="flex flex-row justify-center">
+                                <a href="{{ route('mjrv.export') }}"
+                                    class="block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                    Generar Reporte</a>
+                            </div>
                         </div>
-                    </div>
+                    @endif
+
                 </div>
             </div>
         </div>
