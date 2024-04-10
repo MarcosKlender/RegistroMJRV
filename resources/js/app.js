57fb81b2289ecf999a46515aff5363d4fade9614
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2024-04-10 18:23:58 -0500

    Toggle button added for dark mode.
    Now you can alternate themes in mobile devices (light/dark theme).
    Warning message added at registration.

--- resources/js/app.js
+++ resources/js/app.js
@@ -49,3 +49,47 @@ themeToggleBtn.addEventListener("click", function () {
         }
     }
 });
+
+// MOBILE DARK MODE BUTTON
+var mthemeToggleDarkIcon = document.getElementById("mtheme-toggle-dark-icon");
+var mthemeToggleLightIcon = document.getElementById("mtheme-toggle-light-icon");
+
+// Change the icons inside the button based on previous settings
+if (
+    localStorage.getItem("color-theme") === "dark" ||
+    (!("color-theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    mthemeToggleLightIcon.classList.remove("hidden");
+} else {
+    mthemeToggleDarkIcon.classList.remove("hidden");
+}
+
+var mthemeToggleBtn = document.getElementById("mtheme-toggle");
+
+mthemeToggleBtn.addEventListener("click", function () {
+    // toggle icons inside button
+    mthemeToggleDarkIcon.classList.toggle("hidden");
+    mthemeToggleLightIcon.classList.toggle("hidden");
+
+    // if set via local storage previously
+    if (localStorage.getItem("color-theme")) {
+        if (localStorage.getItem("color-theme") === "light") {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        }
+
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("color-theme", "light");
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("color-theme", "dark");
+        }
+    }
+});
