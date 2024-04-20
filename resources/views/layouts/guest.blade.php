d5166b625dbccc079068a96047d8242364103787
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2024-04-19 23:01:05 -0500

    Login UI updated.
    App info added.
    Visual fixes.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
+<!--
+Diseñado y desarrollado por Marcos Carrasco.
+https://www.linkedin.com/in/marcosklender/
+-->
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -27,15 +32,25 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col items-center justify-center pt-0 bg-gray-100 dark:bg-gray-900">
-        <div style="width:250px;height:200px;">
+    <div class="min-h-screen flex flex-col items-center justify-center pt-0 pb-6 bg-gray-100 dark:bg-gray-900">
+        <div style="width:200px;height:140ox;">
             <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
         </div>
+
+        <h1 class="my-5 text-4xl font-extrabold dark:text-white">ASISTENCIA MJRV</h1>
 
         <div
             class="w-full sm:max-w-md mt-0 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
             {{ $slot }}
         </div>
+
+        <footer
+            class="hidden md:inline fixed bottom-0 left-0 z-20 w-full px-6 py-2 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between dark:bg-gray-800 dark:border-gray-600">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 CNE SD. Todos los derechos
+                reservados.</span>
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">v1.0.0</span>
+        </footer>
+
     </div>
 </body>
 
